752666eb9b4b315c2ee19fa691d7ab3244c2aa0d
TheoDaix <theodaix@hotmail.com>
2018-11-14 17:12:06 +0100

    Fin commentaire TP2

--- TP2 (interpolation et moindres carrés)/TP2-1.1/src/q1.py
+++ TP2 (interpolation et moindres carrés)/TP2-1.1/src/q1.py
@@ -3,35 +3,49 @@ import numpy as np
 from sys import argv as ldc
 
 def solution_systeme(A, B):
+    """
+    Permet de resoudre un système Ax = B ou A est un produit d'une matrice
+    avec sa transposee.
+    """
     sol = sll.dgesv(A, B)
     if sol[3] == 0 :
         return sol[2]
     elif sol[3] > 0:
-        raise Exception("Le système comporte une infinité de solutions ou est impossible.")
+        raise Exception("Le systeme comporte une infinite de solutions ou \
+                        est impossible.")
     else:
         raise Exception("Un des arguments n'est pas valide.")
 
 def formMat(args):
+    """
+    Permet de former les matrices a partir des arguments entres en ligne de
+    commande. Retourne le tuple (M, N, A, B) ou A est une matrice M * N et B
+    une matrice M * 1.
+    """
     M = int(args[0])
     N = int(args[1])
     compteur = 2
     A = []
     B = []
-    for i in range(0, M):
-        ligne = []
-        for j in range(0, N):
-            ligne.append(float(args[compteur]))
+    nbargs = 2 + M * N + M
+    if len(args) == nbargs:
+        for i in range(0, M):
+            ligne = []
+            for j in range(0, N):
+                ligne.append(float(args[compteur]))
+                compteur += 1
+            A.append(ligne)
+        for i in range(0, M):
+            B.append([float(args[compteur])])
             compteur += 1
-        A.append(ligne)
-    for i in range(0, M):
-        B.append([float(args[compteur])])
-        compteur += 1
-    return (M, N, A, B)
+        return (M, N, A, B)
+    else:
+        raise Exception("Le nombre d'argument(s) rentre(s) est incorrect.")
 
 if __name__ == '__main__':
     """
-    Methode "main", elle lancera la methode principale avec la valeur donnee en
-    ligne de commande.
+    Methode "main", elle lancera la methode principale avec les valeurs
+    donnees en ligne de commande.
     """
     (M, N, A, b) = formMat(ldc[1:])
     At = np.transpose(A)
--- TP2 (interpolation et moindres carrés)/TP2-1.1/src/q2.py
+++ TP2 (interpolation et moindres carrés)/TP2-1.1/src/q2.py
@@ -1,14 +1,14 @@
-import scipy.linalg.blas as s
+import scipy.linalg.blas as slb
 import numpy as np
-from q1 import solution_systeme as q1
+import q1
 
 def deriv(A, dA, B, dB, t):
     """
-    Fonction deriv qui prend en paramètres les matrices A, B qui varient en
-    fonction de t ainsi que leur dérivées respectives dA et dB.
-    On évalue d'abord les 4 matrices au moment t ; on se ramène ensuite à
-    calculer la solution premier système et finalement, après une série de
-    manipulations, à calculer la solution d'un second système.
+    Fonction deriv qui prend en parametres les matrices A, B qui varient en
+    fonction de t ainsi que leur derivees respectives dA et dB.
+    On evalue d'abord les 4 matrices au moment t ; on se ramene ensuite a
+    calculer la solution premier systeme et finalement, après une serie de
+    manipulations, a calculer la solution d'un second systeme.
     """
     a = A(t)
     da = dA(t)
@@ -17,34 +17,21 @@ def deriv(A, dA, B, dB, t):
     at = np.transpose(a)
     dat = np.transpose(da)
 
-    m = s.sgemm(1, at, a)
-    m1 = s.sgemm(1, at, b)
+    m = slb.sgemm(1, at, a)
+    m1 = slb.sgemm(1, at, b)
 
-    x = q1(m, m1)
+    x = q1.solution_systeme(m, m1)
 
-    m1 = s.sgemm(1, dat, a)
-    m1 = s.sgemm(1, m1, x)
-    m2 = s.sgemm(1, at, da)
-    m2 = s.sgemm(1, m2, x)
-    m1 = sum(1, m1, m2)
+    m1 = slb.sgemm(1, dat, a)
+    m1 = slb.sgemm(1, m1, x)
+    m2 = slb.sgemm(1, at, da)
+    m2 = slb.sgemm(1, m2, x)
+    m1 = slb.saxpy(m1, m2)
 
-    m2 = s.sgemm(1, dat, b)
-    m3 = s.sgemm(1, at, db)
-    m2 = sum(1, m2, m3)
+    m2 = slb.sgemm(1, dat, b)
+    m3 = slb.sgemm(1, at, db)
+    m2 = slb.saxpy(m2, m3)
 
-    m2 = sum(-1, m1, m2)
-    return q1(m, m2)
-
-def sum(alpha, A, B):
-    """
-    Calcule la somme de deux matrices A et B données en paramètres.
-    """
-    n = len(A)
-    m = len(A[0])
-    S = []
-    for i in range(0, n):
-        ligne = []
-        for j in range(0,m):
-            ligne.append(A[i][j] + alpha * B[i][j])
-        S.append(ligne)
-    return S
+    m1 = slb.sscal(-1, m1)
+    m2 = slb.saxpy(m1, m2)
+    return q1.solution_systeme(m, m2)
