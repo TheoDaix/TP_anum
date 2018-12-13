34db9e915f984f964ed8245e8a2a5dc039d515e0
TheoDaix <theodaix@hotmail.com>
2018-12-13 09:18:28 +0100

    Quelques modifs

--- TP3 (EDO)/TP3-1.1/src/time_p1.py
+++ TP3 (EDO)/TP3-1.1/src/time_p1.py
@@ -1,8 +1,6 @@
 from math import sin, exp, cos, pi
-import trajectoire
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
+import numpy as np, scipy.optimize as spo, matplotlib.pyplot as plt
+import sys, trajectoire
 
 def gamma(s, a):
     return (s, (1 + cos(s)) * exp(-a * s))
@@ -14,36 +12,57 @@ def ddgamma(s, a):
     return (0, a * exp(-a * s) * (sin(s) + a * (1 + cos(s))) - exp(-a * s) *
             (cos(s) - a * sin(s)))
 
+def S(a, t, mu, y0):
+    res = trajectoire.solution(lambda x: dgamma(x, a), lambda x: ddgamma(x, a),
+                               mu, m, t, 2, y0)
+    return res[1][0] - pi
+
 def time_p1(m, a, mu):
     y0 = (0, 0)
     t = 0
-    res = trajectoire.solution(lambda x : dgamma(x, a), lambda x : ddgamma(x,
-     a), mu, m, t, 2, y0)
+    res = trajectoire.solution(lambda x: dgamma(x, a), lambda x: ddgamma(x, a),
+                               mu, m, t, 2, y0)
     while(res[1][0] < pi):
         t += 0.1
-        res = trajectoire.solution(lambda x: dgamma(x, a), lambda x: ddgamma(x,
-                                a), mu, m, t, 2, y0)
+        res = trajectoire.solution(lambda x: dgamma(x, a),
+                                   lambda x: ddgamma(x, a),
+                                   mu, m, t, 2, y0)
     val = res[1][0]
     ecart = t/2
-    while(abs(val - pi)/pi > 0.000000001):
+    while(abs(val - pi)/pi >= 1e-8):
         ecart = ecart / 2
         if (val < pi):
             t = t + ecart
         else:
             t = t - ecart
-        res = trajectoire.solution(lambda x: dgamma(x, a), lambda x: ddgamma(
-            x, a), mu, m, t, 2, y0)
+        res = trajectoire.solution(lambda x: dgamma(x, a),
+                                   lambda x: ddgamma(x, a),
+                                   mu, m, t, 2, y0)
         val = res[1][0]
-    print(t)
+    return t
 
-    res = trajectoire.solution(lambda x : dgamma(x, a), lambda x : ddgamma(x,
-     a), mu, m, t, 101, y0)
+def time_p1_elegant(m, a, mu):
+    y0 = (0, 0)
+    t = 0
+    res = trajectoire.solution(lambda x: dgamma(x, a), lambda x: ddgamma(x, a),
+                               mu, m, t, 2, y0)
+    while (res[1][0] < pi):
+        t += 0.1
+        res = trajectoire.solution(lambda x: dgamma(x, a),
+                                   lambda x: ddgamma(x, a),
+                                   mu, m, t, 2, y0)
+    t = spo.brentq(lambda x: S(a, x, mu, y0), 0, t)
+    return t
+
+def graphe(m, mu, t, y0, a):
+    res = trajectoire.solution(lambda x: dgamma(x, a), lambda x: ddgamma(x, a),
+                               mu, m, t, 101, y0)
     t = np.linspace(0, t, 101)
     plt.plot(t, res[:, 0], 'b', label='S(t)')
     plt.plot(t, res[:, 1], 'g', label='dS(t)')
     for j in res:
         i = gamma(j[0], a)
-        plt.plot(i[0], i[1], 'ro', label='gamma(S(t))')
+        plt.plot(i[0], i[1], 'ro')
     plt.legend(loc='best')
     plt.xlabel('t')
     plt.grid()
@@ -52,4 +71,8 @@ def time_p1(m, a, mu):
 if __name__ == "__main__":
     m = float(sys.argv[1])
     a = float(sys.argv[2])
-    time_p1(m, a, 0.3)
+    #t1 = time_p1(m, a, 0.3)
+    t2 = time_p1_elegant(m, a, 0.3)
+    print(t2)
+    #print(abs(t1 - t2) < 1e-8)
+    graphe(m, 0.3, t2, (0, 0), a)
