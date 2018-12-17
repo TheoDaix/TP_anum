2d3e522b145c0b03f09d5eda172981c04bf764f4
VictorV <victorverhoye@gmail.com>
2018-12-17 20:01:35 +0100

    test sup mu

--- TP3 (EDO)/TP3-1.1/src/sup_mu.py
+++ TP3 (EDO)/TP3-1.1/src/sup_mu.py
@@ -4,7 +4,7 @@ from math import pi
 
 def sup_mu(m, a, t):
     mu = 0
-    arret = True
+    arret = True #inutile si on garde ça
     while (arret):
         res = trajectoire.solution(lambda x: tp.dgamma(x, a),
                                    lambda x: tp.ddgamma(x, a),
@@ -28,7 +28,7 @@ def sup_mu(m, a, t):
     if (mu == 0):
         return "AUCUN"
     else:
-        return recherche_mu(m, (0, 0), a, mu - 0.01, mu, t)
+        return recherche_mu_boucle(m, (0, 0), a, mu - 0.01, mu, t)
 
 def recherche_mu(m, y0, a, mu0, mu, t):
     milieu = (mu0 + mu)/2
@@ -48,14 +48,23 @@ def recherche_mu(m, y0, a, mu0, mu, t):
                                    milieu, m, t, 2, y0)
     """
 
-    if (abs(mu - mu0) < 1e-8):
+    if (abs(mu - mu0)/abs(mu) < 1e-8):
         return milieu
-
     if (res[1][0] <= pi):
         return recherche_mu(m, y0, a, mu0, milieu, t)
     else:
         return recherche_mu(m, y0, a, milieu, mu, t)
-
+def recherche_mu_boucle(m, y0, a, mu0, mu, t):
+    while (abs(mu - mu0)/mu0 > 1e-8):
+        milieu = (mu0 + mu)/2
+        res = trajectoire.solution(lambda x: tp.dgamma(x, a),
+                                   lambda x: tp.ddgamma(x, a),
+                                   milieu, m, t, 2, y0)
+        if (res[1][0] <= pi):
+             mu = milieu
+        else:
+            mu0 = milieu
+    return milieu
 if __name__ == "__main__":
     m = float(sys.argv[1])
     a = float(sys.argv[2])
